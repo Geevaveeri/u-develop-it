42be03d15b9d96e525066846dcfa169e352c892f
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-05-28 17:32:08 -0600

    created votes table

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: process.env.DB_PASS,
+  database: 'election'
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const db = require("./db/connections");
 const express = require('express');
-const apiRoutes = require("./routes/apiRoutes");
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,18 +10,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Use apiRoutes
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 // Start server after DB connection
 db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
