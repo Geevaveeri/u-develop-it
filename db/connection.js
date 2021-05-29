991e292a61d49f0011da1b6570072cb6507aa9c4
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-05-28 18:37:19 -0600

    final commit

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const db = mysql.createConnection({
   host: 'localhost',
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: process.env.DB_PASS,
+  password: '',
   database: 'election'
 });
 
